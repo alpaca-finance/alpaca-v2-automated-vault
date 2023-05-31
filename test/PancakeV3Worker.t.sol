0485a0223f7cb23d5ea33ba8c59a47ab478c7916
alpanaca <rblue4594@gmail.com>
2023-05-31 14:02:50 +0700

    feat: add transfer to worker

--- test/PancakeV3Worker.t.sol
+++ test/PancakeV3Worker.t.sol
@@ -285,3 +285,31 @@ contract PancakeV3Worker_ReinvestTest is BasePancakeV3WorkerTest {
     worker.reinvest();
   }
 }
+
+contract PancakeV3Worker_TransferTest is BasePancakeV3WorkerTest {
+  constructor() BasePancakeV3WorkerTest() { }
+
+  function testCorrectness_WhenTransferBackToExecutor_ShouldWork() external {
+    uint256 _amount = 1 ether;
+    deal(address(token1), address(worker), _amount);
+    uint256 _workerToken1AmountBefore = token1.balanceOf(address(worker));
+    uint256 _executorToken1AmountBefore = token1.balanceOf(IN_SCOPE_EXECUTOR);
+    vm.prank(IN_SCOPE_EXECUTOR);
+    worker.transfer(address(token1), IN_SCOPE_EXECUTOR, _amount);
+
+    assertEq(token1.balanceOf(address(worker)), _workerToken1AmountBefore - _amount);
+    assertEq(token1.balanceOf(address(IN_SCOPE_EXECUTOR)), _executorToken1AmountBefore + _amount);
+  }
+
+  function testRevert_WhenDestinationAddressIsZero() external {
+    vm.prank(IN_SCOPE_EXECUTOR);
+    vm.expectRevert();
+    worker.transfer(address(token1), address(0), 1 ether);
+  }
+
+  function testRevert_WhenTransferAmountIsZero() external {
+    vm.prank(IN_SCOPE_EXECUTOR);
+    vm.expectRevert();
+    worker.transfer(address(token1), IN_SCOPE_EXECUTOR, 0);
+  }
+}
