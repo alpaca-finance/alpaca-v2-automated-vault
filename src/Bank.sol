2110a63a8f6f760e8eb7c6c5e9ad13ffb74dcf1e
69Alpacas <alpaca69.dev@gmail.com>
2023-06-19 18:08:09 +0700

    refactor: use oz enum set instead of linked list

--- src/Bank.sol
+++ src/Bank.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.19;
 import { ERC20 } from "@solmate/tokens/ERC20.sol";
 import { SafeTransferLib } from "@solmate/utils/SafeTransferLib.sol";
 import { SafeCastLib } from "@solmate/utils/SafeCastLib.sol";
+import { EnumerableSet } from "@openzeppelin/utils/structs/EnumerableSet.sol";
 import { Initializable } from "@openzeppelin-upgradeable/proxy/utils/Initializable.sol";
 import { Ownable2StepUpgradeable } from "@openzeppelin-upgradeable/access/Ownable2StepUpgradeable.sol";
 import { ReentrancyGuardUpgradeable } from "@openzeppelin-upgradeable/security/ReentrancyGuardUpgradeable.sol";
@@ -15,22 +16,23 @@ import { AutomatedVaultManager } from "src/AutomatedVaultManager.sol";
 
 // libraries
 import { LibShareUtil } from "src/libraries/LibShareUtil.sol";
-import { LibVaultDebt } from "src/libraries/LibVaultDebt.sol";
 
 contract Bank is Initializable, Ownable2StepUpgradeable, ReentrancyGuardUpgradeable {
   using SafeCastLib for uint256;
   using SafeTransferLib for ERC20;
   using LibShareUtil for uint256;
-  using LibVaultDebt for mapping(address => LibVaultDebt.VaultDebtList);
+  using EnumerableSet for EnumerableSet.AddressSet;
 
   error Bank_ExecutorNotInScope();
-
-  // vault token => VaultDebtList
-  mapping(address => LibVaultDebt.VaultDebtList) public vaultDebtLists;
+  error Bank_RepayMoreThanDebt();
 
   IMoneyMarket public moneyMarket;
   AutomatedVaultManager public vaultManager;
 
+  // vault token => list of borrowed tokens
+  mapping(address => EnumerableSet.AddressSet) internal vaultDebtTokens;
+  // vault token => borrowed token => debt shares
+  mapping(address => mapping(address => uint256)) public vaultDebtShares;
   // token => total debt shares
   mapping(address => uint256) public tokenDebtShares;
 
@@ -60,11 +62,9 @@ contract Bank is Initializable, Ownable2StepUpgradeable, ReentrancyGuardUpgradea
   }
 
   function accrueInterest(address _vaultToken) external {
-    uint256 _length = vaultDebtLists.getLength(_vaultToken);
-    address _currentToken = LibVaultDebt.START;
+    uint256 _length = vaultDebtTokens[_vaultToken].length();
     for (uint256 _i; _i < _length;) {
-      _currentToken = vaultDebtLists.getNextOf(_vaultToken, _currentToken);
-      moneyMarket.accrueInterest(_currentToken);
+      moneyMarket.accrueInterest(vaultDebtTokens[_vaultToken].at(_i));
       unchecked {
         ++_i;
       }
@@ -76,7 +76,7 @@ contract Bank is Initializable, Ownable2StepUpgradeable, ReentrancyGuardUpgradea
     view
     returns (uint256 _debtShares, uint256 _debtAmount)
   {
-    _debtShares = vaultDebtLists.getDebtSharesOf(_vaultToken, _token);
+    _debtShares = vaultDebtShares[_vaultToken][_token];
     // NOTE: must accrue interest on money market before calculate shares to correctly reflect debt
     _debtAmount =
       _debtShares.shareToValue(moneyMarket.getNonCollatAccountDebt(address(this), _token), tokenDebtShares[_token]);
@@ -89,17 +89,21 @@ contract Bank is Initializable, Ownable2StepUpgradeable, ReentrancyGuardUpgradea
     _moneyMarket.accrueInterest(_token);
 
     // Effects
+    // Cache to save gas
+    uint256 _cachedTokenDebtShares = tokenDebtShares[_token];
+    // NOTE: must accrue interest on money market before calculate shares to correctly reflect debt
+    // Round up in protocol favor
+    uint256 _debtSharesToAdd = _amount.valueToShareRoundingUp(
+      _cachedTokenDebtShares, _moneyMarket.getNonCollatAccountDebt(address(this), _token)
+    );
+    // Add to borrowed token list if borrow for first time
+    if (_cachedTokenDebtShares == 0) {
+      vaultDebtTokens[_vaultToken].add(_token);
+    }
     // Safe to use unchecked since overflow amount would revert on borrow or transfer anyway
     unchecked {
-      // Cache to save gas
-      uint256 _cachedTokenDebtShares = tokenDebtShares[_token];
-      // NOTE: must accrue interest on money market before calculate shares to correctly reflect debt
-      // Round up in protocol favor
-      uint256 _debtSharesToAdd = _amount.valueToShareRoundingUp(
-        _cachedTokenDebtShares, _moneyMarket.getNonCollatAccountDebt(address(this), _token)
-      );
       tokenDebtShares[_token] = _cachedTokenDebtShares + _debtSharesToAdd;
-      vaultDebtLists.increaseDebtSharesOf(_vaultToken, _token, _debtSharesToAdd);
+      vaultDebtShares[_vaultToken][_token] += _debtSharesToAdd;
     }
 
     // Interactions
@@ -127,9 +131,18 @@ contract Bank is Initializable, Ownable2StepUpgradeable, ReentrancyGuardUpgradea
     // Round down in protocol favor
     uint256 _debtSharesToDecrease =
       _amount.valueToShare(_cachedTokenDebtShares, _moneyMarket.getNonCollatAccountDebt(address(this), _token));
-    // Will revert underflow if repay more than debt
-    tokenDebtShares[_token] = _cachedTokenDebtShares - _debtSharesToDecrease;
-    vaultDebtLists.decreaseDebtSharesOf(_vaultToken, _token, _debtSharesToDecrease);
+    if (_debtSharesToDecrease > _cachedTokenDebtShares) {
+      revert Bank_RepayMoreThanDebt();
+    }
+    // Safe to unchecked, already checked above
+    unchecked {
+      tokenDebtShares[_token] = _cachedTokenDebtShares - _debtSharesToDecrease;
+      // Remove from borrowed token list if repay all
+      if (_cachedTokenDebtShares - _debtSharesToDecrease == 0) {
+        vaultDebtTokens[_vaultToken].remove(_token);
+      }
+    }
+    vaultDebtShares[_vaultToken][_token] -= _debtSharesToDecrease;
 
     // Interactions
     ERC20(_token).safeApprove(address(_moneyMarket), _amount);
