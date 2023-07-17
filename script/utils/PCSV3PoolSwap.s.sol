418ab0cdd04037b67de66231fb869d2616d77b69
jr-alpaca <uviveramrom@gmail.com>
2023-07-17 13:38:01 +0700

    chore: script for swap on pscv3

--- script/utils/PCSV3PoolSwap.s.sol
+++ script/utils/PCSV3PoolSwap.s.sol
@@ -4,11 +4,9 @@ pragma solidity 0.8.19;
 import "script/BaseScript.sol";
 
 import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
-import { IPancakeSwapRouterV3 } from
-  "lib/alpaca-v2-money-market/solidity/contracts/money-market/interfaces/IPancakeSwapRouterV3.sol";
+import { IPancakeV3Router } from "src/interfaces/pancake-v3/IPancakeV3Router.sol";
 
 contract PCSV3PoolSwapScript is BaseScript {
-  address internal constant PANCAKE_V3_ROUTER = 0x13f4EA83D0bd40E75C8222255bc855a974568Dd4;
 
   function run() public {
     address _tokenIn = usdt;
@@ -16,18 +14,16 @@ contract PCSV3PoolSwapScript is BaseScript {
     uint24 _fee = 500;
     uint256 _amountIn = 10000;
 
-    vm.startBroadcast(deployerPrivateKey);
-
-    IPancakeSwapRouterV3.ExactInputParams memory params = IPancakeSwapRouterV3.ExactInputParams({
+    IPancakeV3Router.ExactInputParams memory params = IPancakeV3Router.ExactInputParams({
       path: abi.encodePacked(_tokenIn, _fee, _tokenOut),
       recipient: vm.addr(deployerPrivateKey),
-      deadline: 10000000,
       amountIn: _amountIn,
       amountOutMinimum: 0
     });
+    vm.startBroadcast(deployerPrivateKey);
 
-    ERC20(_tokenIn).approve(PANCAKE_V3_ROUTER, _amountIn);
-    IPancakeSwapRouterV3(PANCAKE_V3_ROUTER).exactInput(params);
+    ERC20(_tokenIn).approve(pancakeV3Router, _amountIn);
+    IPancakeV3Router(pancakeV3Router).exactInput(params);
 
     vm.stopBroadcast();
   }
