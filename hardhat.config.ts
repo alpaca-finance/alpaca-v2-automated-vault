10eaf63f8566c14248ad490b408919bfca6b16e2
jr-alpaca <uviveramrom@gmail.com>
2023-07-24 13:08:29 +0700

    chore: typechain package

---
+++ hardhat.config.ts
@@ -0,0 +1,43 @@
+import { config as dotEnvConfig } from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+dotEnvConfig();
+
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    bsc_mainnet: {
+      chainId: 56,
+      url: process.env.BSC_RPC_URL,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
+  },
+  paths: {
+    sources: "./solidity/contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  typechain: {
+    outDir: "./typechain",
+    target: "ethers-v5",
+  },
+  mocha: {
+    timeout: 100000,
+  },
+};
+
+export default config;
