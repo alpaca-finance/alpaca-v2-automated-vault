ab11465e12ce931a36d2e05acfc2d5aa08d43485
jr-alpaca <uviveramrom@gmail.com>
2023-07-26 11:56:13 +0700

    chore: open vault script

--- deploy/av-manager/config/open-vault.ts
+++ deploy/av-manager/config/open-vault.ts
@@ -2,7 +2,7 @@ import { AutomatedVaultManager__factory } from "./../../../typechain/factories/s
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { ConfigFileHelper } from "../../file-helper/config-file-helper";
-import { getDeployer } from "../../utils/deployer-helper";
+import { getDeployer, isFork } from "../../utils/deployer-helper";
 import { compare } from "../../utils/address";
 
 interface OpenVaultParams {
@@ -29,7 +29,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const NAME = "Saving USDT-BNB 250 PCS1";
+  const NAME = "Saving USDT-BNB-250 PCS1";
   const SYMBOL = "L-USDTBNB250-PCS1";
   const param: OpenVaultParams = {
     worker: "",
@@ -39,9 +39,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     compressedCapacity: 500_000, // 500,000 USD
     managementFeePerSec: 634195840, // 2% per year
     withdrawalFeeBps: 20,
-    toleranceBps: 25,
+    toleranceBps: 9975, // inverse 25 bps
     maxLeverage: 8,
   };
+  const VAULT_MANAGER = "";
+  const INVEST_TOKEN = config.tokens.wbnb;
 
   console.log("Open Vault param", param);
 
@@ -51,13 +53,33 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     config.automatedVault.automatedVaultManager.proxy,
     deployer
   );
+  const ops = isFork() ? { gasLimit: 2000000 } : {};
 
-  await automatedVaultManager.openVault(NAME, SYMBOL, param);
+  const newVaultToken = await automatedVaultManager.callStatic.openVault(NAME, SYMBOL, param, ops);
 
-  const vaultConfig = config.automatedVault.pancakeV3Vault.vaults.find((vault) => compare(vault.worker, param.worker));
+  const openVaultTx = await automatedVaultManager.openVault(NAME, SYMBOL, param, ops);
+  const openVaultReceipt = await openVaultTx.wait();
 
-  if (vaultConfig) {
-    configFileHelper.addOrSetPCSV3VaultByWorker(vaultConfig);
+  if (openVaultReceipt.status === 1) {
+    console.log(`> 🟢 Done Open Vualt: ${openVaultReceipt.transactionHash}`);
+    const vaultConfig = config.automatedVault.pancakeV3Vault.vaults.find((vault) =>
+      compare(vault.worker, param.worker)
+    );
+
+    if (vaultConfig) {
+      vaultConfig.name = NAME;
+      vaultConfig.symbol = SYMBOL;
+      vaultConfig.vaultToken = newVaultToken;
+      configFileHelper.addOrSetPCSV3VaultByWorker(vaultConfig);
+    }
+
+    console.log("> Setting Allow Token ...");
+    const setAllowTokenTx = await automatedVaultManager.setAllowToken(newVaultToken, INVEST_TOKEN, true);
+    console.log(`> 🟢 Done Setting Allow Token: ${setAllowTokenTx.hash}`);
+
+    console.log("> Setting Vault Manager ...");
+    const setVaultManagetTx = await automatedVaultManager.setVaultManager(newVaultToken, VAULT_MANAGER, true);
+    console.log(`> 🟢 Done Setting Vault Manager: ${setVaultManagetTx.hash}`);
   }
 };
 
